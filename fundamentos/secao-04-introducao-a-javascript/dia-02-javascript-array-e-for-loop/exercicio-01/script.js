9832e30a74e0a2611768b02ae06de6997dd2a641
Daniel Pinheiro Machado <dyelzim@gmail.com>
2022-11-26 14:07:39 -0300

    finished exercises array for

---
+++ fundamentos/secao-04-introducao-a-javascript/dia-02-javascript-array-e-for-loop/exercicio-01/script.js
@@ -0,0 +1,58 @@
+// let tasksList = ["Tomar café", "Reunião", "Brincar com o cachorro"];
+
+// console.log(tasksList.length);
+
+// console.log(tasksList[0]);
+
+// console.log(tasksList[tasksList.length - 1]);
+
+// tasksList.push("Fazer exercícios da Trybe");
+
+// console.log(tasksList);
+
+// tasksList.pop();
+// console.log(tasksList);
+
+// let menu = ["Home", "Serviços", "Portfólio", "Links"];
+// let menuServices = menu[1];
+
+// console.log(menuServices);
+
+// console.log(menu.indexOf("Portfólio"));
+
+// menu.push("Contato");
+// console.log(menu);
+
+// let cars = ["Saab", "Volvo", "BMW"];
+
+// for (let index = 0; index < cars.length; index++) {
+//   const element = cars[index];
+//   console.log(element);
+// }
+
+// let groceryList = ["Arroz", "Feijão", "Alface", "Melancia"];
+
+// for (let index = 0; index < groceryList.length; index++) {
+//   const element = groceryList[index];
+//   console.log("");
+//   console.log(element);
+// }
+
+// let names = ["João", "Maria", "Antonio", "Margarida"];
+// console.log(names.length);
+
+// for (let name of names) {
+//   console.log("Olá", name);
+// }
+
+// for (let index = 0; index <= 20; index += 1) {
+//   console.log(index);
+// }
+
+for (let index = 0; index < 10; index += 1) {
+  console.log("Oie", index);
+}
+
+for (let index = 0; index < 20; index += 2) {
+  console.log("Oie", index);
+}
---
+++ fundamentos/secao-04-introducao-a-javascript/dia-02-javascript-array-e-for-loop/exercicio-02/script.js
@@ -0,0 +1,80 @@
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// for (let index = 0; index < numbers.length; index += 1) {
+//   const element = numbers[index];
+//   console.log(element);
+// }
+// var sumElements = 0;
+// for (let index = 0; index < numbers.length; index += 1) {
+//   sumElements += numbers[index];
+// }
+// console.log(sumElements);
+
+// var sumNumbers = 0;
+// for (let number of numbers) {
+//   sumNumbers += number;
+// }
+
+// console.log(sumNumbers);
+
+// var sumElements = 0;
+// for (let index = 0; index < numbers.length; index += 1) {
+//   sumElements += numbers[index];
+// }
+// let mediaAritmetica = sumElements / numbers.length;
+// console.log(mediaAritmetica);
+
+// if (mediaAritmetica > 20) {
+//   console.log("Valor maior que 20");
+// } else {
+//   console.log("Valor menor que 20");
+// }
+
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// var qtdImpares = 0;
+
+// for (let index = 0; index < numbers.length; index += 1) {
+//   if (numbers[index] % 2 != 0) {
+//     qtdImpares += 1;
+//   }
+// }
+// if (qtdImpares > 0) {
+//   console.log("Quantidade de números ímpares:", qtdImpares);
+// } else {
+//   console.log("Nenhum valor ímpar encontrado.");
+// }
+
+// let listTwentyFive = [];
+// for (let index = 1; index <= 25; index++) {
+//   listTwentyFive.push(index);
+// }
+// console.log(listTwentyFive);
+
+// for (let index = 0; index < listTwentyFive.length; index++) {
+//   console.log(listTwentyFive[index] / 2);
+// }
+
+//maior valor contido num array
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// var maxNumberArray = numbers[0];
+// for (let index = 0; index < numbers.length; index++) {
+//   if (numbers[index] > maxNumberArray) {
+//     maxNumberArray = numbers[index];
+//   }
+// }
+
+// console.log(maxNumberArray);
+
+//menor valor contido num array
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// var minNumberArray = numbers[0];
+// for (let index = 0; index < numbers.length; index++) {
+//   if (numbers[index] < minNumberArray) {
+//     minNumberArray = numbers[index];
+//   }
+// }
+
+// console.log(minNumberArray);
