0d91360c4933d413821693f6deb7a7e6cdd00369
Daniel Pinheiro Machado <dyelzim@gmail.com>
2022-12-20 19:44:29 -0300

    exercise 5.3 part 1

---
+++ fundamentos/secao-05-dom-eventos-web-storage/dia03-eventos/exercises1/script.js
@@ -0,0 +1,46 @@
+const createDaysOfTheWeek = () => {
+  const weekDays = [
+    'Domingo',
+    'Segunda',
+    'Terça',
+    'Quarta',
+    'Quinta',
+    'Sexta',
+    'Sábado',
+  ];
+  const weekDaysList = document.querySelector('.week-days');
+
+  for (let index = 0; index < weekDays.length; index += 1) {
+    const days = weekDays[index];
+    const dayListItem = document.createElement('li');
+    dayListItem.innerHTML = days;
+
+    weekDaysList.appendChild(dayListItem);
+  }
+};
+
+createDaysOfTheWeek();
+
+//parte1
+const createDaysOfTheMonth = () => {
+  const decemberDaysList = [
+    29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
+    20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
+  ];
+
+  const monthDayslist = document.querySelector('#days');
+
+  for (let index = 0; index <= decemberDaysList.length; index += 1) {
+    const dayNumber = decemberDaysList[index];
+    const elementLi = document.createElement('li');
+    elementLi.innerHTML = dayNumber;
+    dayNumber === 24 || dayNumber === 25 || dayNumber === 31
+      ? (elementLi.className = 'day holiday')
+      : (elementLi.className = 'day');
+    dayNumber === 4 || dayNumber === 11 || dayNumber === 18 || dayNumber === 25
+      ? (elementLi.className = 'day friday')
+      : (elementLi.className = 'day');
+    monthDayslist.appendChild(elementLi);
+  }
+};
+createDaysOfTheMonth();
